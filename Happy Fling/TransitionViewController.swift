10dd7e0384c720396f8211f2c5ada4bef2641a49
ZHANG Lunjie <Lunjie@129-046.eduroam.rwth-aachen.de>
2015-01-20 00:02:48 +0800

    Add shape and color transition screen.

---
+++ Happy Fling/TransitionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TransitionViewController.swift
+//  Happy Fling
+//
+//  Created by ZHANG Lunjie on 1/19/15.
+//  Copyright (c) 2015 JJJLM. All rights reserved.
+//
+
+import UIKit
+
+class TransitionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
