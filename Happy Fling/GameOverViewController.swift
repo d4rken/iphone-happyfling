10f601f33ded0f9841277f4fe1cb81f60d9c8dc9
jiseungshin <mailfriend88@gmail.com>
2015-01-23 10:13:26 +0100

    Added Highscoredatabase and gameoverViewcontroller, both files  has still to modified

---
+++ Happy Fling/GameOverViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameOverViewController.swift
+//  Happy Fling
+//
+//  Created by Shin on 2015. 1. 23..
+//  Copyright (c) 2015년 JJJLM. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+
+
+class GameOverViewController: UIViewController {
+    
+    
+    
+}
